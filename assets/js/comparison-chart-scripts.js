3da1ae1ab766e0d0761d6e0acfbfcd6cdf979bb8
Max Baun <max@d3applications.com>
2017-10-02 14:21:58 -0400

    hide the rows with the same data when comparing

--- assets/js/comparison-chart-scripts.js
+++ assets/js/comparison-chart-scripts.js
@@ -20,6 +20,39 @@ $(function() {
       $chartTable.addClass('col-' + (optionCount - this.getAttribute('data-option-index')));
     });
     $('.comparison-chart').addClass('comparison-active');
+
+    // Show all of the rows in the table
+    $('.comparison-chart table tbody tr').show();
+
+    // Loop through all of the rows in the table to find which rows to hide
+    $('.comparison-chart table tbody tr').each(function (index, row) {
+      // Map the tds in the row to an array of objects for comparrable availability
+      var tds = $(row).find('td:visible').map(function (index, td) {
+        return {
+          index: index,
+          html: $(td).html(),
+          classname: $(td).attr('class')
+        }
+      }).toArray();
+
+      // Reduce all of the tds down to see if the row should be visible or not
+      var visible = tds.reduce(function (visible, td) {
+        // Get all of the tds with the same class & html as the current td
+        var found = tds.filter(function (a) {
+          return a.classname === td.classname && a.html === td.html;
+        });
+
+        // If the tds all have the same classname, then set the visibility to hidden
+        if (found.length === tds.length) {
+          visible = false;
+        }
+
+        return visible;
+      }, true);
+
+      return visible ? $(row).show() : $(row).hide();
+    });
+
     $('html, body').animate({ scrollTop: $('.comparison-chart .body-region').offset().top - 15 }, 500);
   });
 
@@ -29,6 +62,8 @@ $(function() {
     $('.comparison-chart .compare-btn').attr('disabled', true);
     $('.comparison-chart').removeClass('comparison-active show-compare-hint');
     $('.comparison-chart table').removeAttr('class');
+    // Show all of the rows in the table
+    $('.comparison-chart table tbody tr').show();
     $('html, body').animate({ scrollTop: $('.page-anchor[id="Compare-Robots"]').offset().top }, 500);
   });
 
