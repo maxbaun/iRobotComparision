fe7eabb1cc1b40f6899829b198882524eaf0e317
Max Baun <max@d3applications.com>
2017-10-03 11:43:28 -0400

    adding a toggle button to show more features on mobile

--- assets/js/comparison-chart-scripts.js
+++ assets/js/comparison-chart-scripts.js
@@ -22,7 +22,7 @@ $(function() {
     $('.comparison-chart').addClass('comparison-active');
 
     // Show all of the rows in the table
-    $('.comparison-chart table tbody tr').show();
+    toggleRows();
 
     $('html, body').animate({ scrollTop: $('.comparison-chart .body-region').offset().top - 15 }, 500);
   });
@@ -33,51 +33,33 @@ $(function() {
     $('.comparison-chart .compare-btn').attr('disabled', true);
     $('.comparison-chart').removeClass('comparison-active show-compare-hint');
     $('.comparison-chart table').removeAttr('class');
-    // Show all of the rows in the table
-    $('.comparison-chart table tbody tr').show();
+    
+    // Reset the collapse button
+    $('.comparison-chart .collapse-btn').attr('data-active', 'false');
+    $('.comparison-chart .collapse-btn').text('Load More Features');
+
+    // Reset the diff button
+    $('.comparison-chart .diff-btn').attr('data-active', 'false');
+    $('.comparison-chart .diff-btn').text('Show Only Differences');   
+    
+    toggleRows();
+
     $('html, body').animate({ scrollTop: $('.page-anchor[id="Compare-Robots"]').offset().top }, 500);
+  });
+
+  $('.comparison-chart .collapse-btn').click(function () {
+    var active = $('.comparison-chart .collapse-btn').attr('data-active');
+    $('.comparison-chart .collapse-btn').attr('data-active', active === 'true' ? 'false' : 'true');
+    $('.comparison-chart .collapse-btn').text(active === 'true' ? 'Load More Features' : 'Show Less Features');
+    toggleRows();
   });
 
   // Show only differences in the chart
   $('.comparison-chart .diff-btn').click(function () {
     var active = $('.comparison-chart .diff-btn').attr('data-active');
-    console.log('active', active)
     $('.comparison-chart .diff-btn').attr('data-active', active === 'true' ? 'false' : 'true');
     $('.comparison-chart .diff-btn').text(active === 'true' ? 'Show Only Differences' : 'Show All Features');
-    // Show all of the rows in the table
-    $('.comparison-chart table tbody tr').show();
-    
-    if (active === 'true') {
-      return;
-    }
-
-    // Loop through all of the rows in the table to find which rows to hide
-    $('.comparison-chart table tbody tr').each(function (index, row) {
-      // Map the tds in the row to an array of objects for comparrable availability
-      var tds = $(row).find('td:visible').map(function (index, td) {
-        return {
-          html: $(td).html(),
-          classname: $(td).attr('class')
-        }
-      }).toArray();
-
-      // Reduce all of the tds down to see if the row should be visible or not
-      var visible = tds.reduce(function (visible, td) {
-        // Get all of the tds with the same class & html as the current td
-        var found = tds.filter(function (a) {
-          return a.classname === td.classname && a.html === td.html;
-        });
-
-        // If the tds all have the same classname, then set the visibility to hidden
-        if (found.length === tds.length) {
-          visible = false;
-        }
-
-        return visible;
-      }, true);
-
-      return visible ? $(row).show() : $(row).hide();
-    });
+    toggleRows();
   });
 
   // Show desktop tooltips
@@ -107,4 +89,85 @@ $(function() {
     // Hide all compare-chart tooltips
     $('.comparison-chart .has-tooltip.active, .comparison-chart .availability.active').removeClass('active');
   });
+
+  $(window).on('resize', function () {
+    toggleCollapseButton();
+    toggleRows();
+  });
+
+  function toggleRows() {
+    var maxRows = getMaxNumRows();
+    var diffActive = $('.comparison-chart .diff-btn').attr('data-active') === 'true';
+
+    if (!diffActive) {
+      $('.comparison-chart table tbody tr:lt(' + maxRows + ')').show();
+      $('.comparison-chart table tbody tr:gt(' + maxRows + ')').hide();
+      return;
+    }
+
+    // Show All Rows
+    $('.comparison-chart table tbody tr').show();
+
+    var visibleRows = [];
+    // Show only differences
+    $('.comparison-chart table tbody tr').each(function (index, row) {
+      // Map the tds in the row to an array of objects for comparrable availability
+      var tds = $(row).find('td:visible')    
+        .map(function (index, td) {
+          return {
+            html: $(td).html(),
+            classname: $(td).attr('class')
+          }
+        })
+        .toArray();
+
+      // Reduce all of the tds down to see if the row should be visible or not
+      var visible = tds.reduce(function (visible, td) {
+        // Get all of the tds with the same class & html as the current td
+        var found = tds.filter(function (a) {
+          return a.classname === td.classname && a.html === td.html;
+        });
+
+        // If the tds all are the same, then set the visibility to hidden
+        if (found.length === tds.length) {
+          visible = false;
+        }
+
+        return visible;
+      }, true);
+
+      if (visible) {
+        visibleRows.push(index);
+      }
+    });    
+
+    // Take only the first X number of rows (5 for mobile & all for desktop)
+    visibleRows = visibleRows.slice(0, maxRows);
+
+    $('.comparison-chart table tbody tr').each(function (index, row) {
+      if (visibleRows.indexOf(index) !== -1) {
+        $(row).show();
+      } else {
+        $(row).hide();
+      }
+    });
+  }
+
+  function getMaxNumRows () {
+    return isMobile() && isCollapsed() ? 5 : $('.comparison-chart table tbody tr').length;
+  }
+
+  function isMobile () {
+    return $(window).width() <= 768;
+  }
+
+  function isCollapsed () {
+    return $('.comparison-chart .collapse-btn').attr('data-active') === 'false';
+  }
+
+  function toggleCollapseButton () {
+    return isMobile() ? $('.comparison-chart .collapse-btn').show() : $('.comparison-chart .collapse-btn').hide();
+  }
+
+  toggleCollapseButton();
 });
